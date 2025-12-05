6987833456c8c87244427f66b79810406499c491
深空超级计算机\qy <tiashanyi7@@outlook.com>
2025-12-05 23:00:26 +0800

    Add vstart = 0x7c00

--- mbr.asm
+++ mbr.asm
@@ -1,11 +1,17 @@
 ;ShenKong OS 1.0
 
+SECTION mbr align=16 vstart=0x7c00
+
+
+
+
+;读取内存测试
 xor ax,ax
 mov ds,ax
-mov ax,ds:[data+0x7c00+0x1]
-mov ds:[data +0x7c00 +0x3],ax
+mov ax,ds:[data+0x1]
+mov ds:[data+0x3],ax
 
-
+;显示字符
 mov ax,0b800h
 mov es,ax
 mov byte es:[0],'w'
@@ -13,16 +19,18 @@ mov byte es:[0],'w'
 ;显示数字
 mov ax,4564
 mov di,160+40
-call showm
+call showNumber
 
+;显示字符的asiic
 mov ax,'a'
 mov di,160*2+40
-call showm
+call showNumber
 
 mov byte es:[2],'o'
-mov byte es:[4],'k'
+mov byte es:[4],'r'
+mov byte es:[6],'k'
 
-mov ax,0x07c0
+mov ax,0
 mov ds,ax
 mov si,text
 mov ax,0xb800
@@ -32,6 +40,15 @@ cld
 mov cx,(textend-text)/2
 rep movsw
 
+;表示负数
+mov al,9
+neg al
+cbw
+cwd
+
+
+
+
 x:
 hlt
 jmp x
@@ -39,10 +56,11 @@ jmp x
 
 ;-------------------------------------------------------------
 
+
 ;展示数字
 ;ax目标数字
 ;di显示偏移
-showm:
+showNumber:
 push si
 push cx
 push dx
@@ -54,22 +72,24 @@ mov cx,10					;除数
 CFSW:
 mov dx,0					;初始化dx用于被除数，ax中有目标数
 div cx
-mov ds:[data2 +0x7c00+si],dl	;存储余数(8bits)(结果会小于8bits)
+;push dx
+mov ds:[data2 +si],dl	;在data2存储余数(8bits)(结果会小于8bits)
 inc si						
 cmp ax,0					;除到ax为0
 jne CFSW
 
 dec si					;减去最后一次增加的1
 ;显示十进制
-tenc:
-mov al,ds:[data2 +0x7c00+si]
+TENC:
+mov al,ds:[data2+si]
+;pop ax
 add al,0x30
 mov es:[di],al
 add di,2
 dec si
 mov ax,si
 cmp ax,-1
-jne tenc
+jne TENC
 
 
 pop dx
@@ -77,8 +97,10 @@ pop cx
 pop si
 ret
 
+
+
 data db 0,1,0,0
-data2 db 0,2,2,2
+data2 db 0,2,2,2,0
 data3 db 'attation'
 text: db 'hhehlhlhoh'
 textend:
